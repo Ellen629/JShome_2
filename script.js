b03f11fa0d5aff91357598807e3311d25472598b
Ellen <elianora123@yahoo.com>
2021-07-02 12:56:08 +0400

    I added some exercises

--- script.js
+++ script.js
@@ -20,11 +20,54 @@
 
 // alert(area);
 
-let array = function(int, count) {
-	let arrayOfMultiples = [];
-  while(i < count){
-  	arrayOfMultiples.push(int * (i + 1));
+// let array = function(int, count) {
+// 	let arrayOfMultiples = [];
+//   while(i < count){
+//   	arrayOfMultiples.push(int * (i + 1));
+//   }
+// }
+// alert(arrayOfMultiples);
+// array(8, 4);
+
+// Edabit first homework
+/* let result = 0;
+function addUp(num){
+  for(let i = 0; i <= num; i++){
+    result = result + i;
   }
 }
-alert(arrayOfMultiples);
-array(8, 4);
+addUp(4);
+console.log(result) */
+
+// Edabit second homework
+/* function numberSplit(int){
+
+}
+numberSplit(4); */
+
+// Edabit third homework
+/* let sum = 0;
+function getAbsSum(array){
+for(let i = 0; i < array.length; i++){
+if(array[i] < 0){
+  sum = sum + (array[i] * -1);
+  continue
+}
+    sum = sum + array[i];
+  }
+  return Math.abs(sum)
+}
+getAbsSum([2, -1, 4, 8, 10])
+console.log(sum) */
+
+
+// Edabit forth homework
+let result = 0;
+function mean(array){
+   for(let i = 0; i < array.length; i++){
+    result = result + array[i];
+  }
+  return result / array.length;
+}
+console.log(mean([2, 3, 2, 3]));
+
